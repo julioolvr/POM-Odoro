7f0a806db085b1379884e05c54793ff351975c12
Julio Olivera <julio.olvr@gmail.com>
2015-07-04 01:43:25 -0300

    First build

--- src/js/index.jsx
+++
@@ -1,5 +0,0 @@
-import Styles from '../css/app.css'; // eslint-disable-line no-unused-vars
-import React from 'react';
-import App from './components/app.jsx';
-
-React.render(<App/>, document.getElementById('app'));
