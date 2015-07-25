4a980b069aa3130dc9cd58bf422f6653c4c3352e
Julio Olivera <julio.olvr@gmail.com>
2015-07-25 19:13:07 -0300

    Start on enter

--- src/js/components/timer.jsx
+++ src/js/components/timer.jsx
@@ -11,11 +11,17 @@ class Timer extends React.Component {
     }
   }
 
+  onInputKeyPress(event) {
+    if (event.key === 'Enter') {
+      this.props.onPomodoroStart();
+    }
+  }
+
   render() {
     return (
       <div>
         <p>
-          Start a <input maxLength="2" autoFocus ref="lengthInput" disabled={this.props.started}
+          Start a <input maxLength="2" autoFocus ref="lengthInput" disabled={this.props.started} onKeyPress={(e) => this.onInputKeyPress(e)}
                  onChange={(e) => this.onPomodoroLengthChange(e)} value={this.props.pomodoroLength}></input> {this.props.pomodoroLength !== 1 ? 'minutes' : 'minute'} pomodoro.
         </p>
         <button className="btn" disabled={this.props.started} onClick={this.props.onPomodoroStart}>Start</button>
